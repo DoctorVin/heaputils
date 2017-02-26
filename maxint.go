d10e2f0bc024b9e7b07f04e8591d8f8913c696cf
Doctor Vince <drvince@Lancer.local>
2017-02-25 22:48:44 -0500

    Add push, pop and sort routines for MaxIntHeaps

--- maxint.go
+++ maxint.go
@@ -1,61 +1,86 @@
 package heaputil
 
-/* For a given index into a slice or array acting as a binary heap, 
+/* For a given index into a slice or array acting as a binary heap,
    calculate the index of the parent node. We are guaranteed of not
    being presented an index less than 3. */
 func getParentIndex(cIdx int) int {
-    var pIdx int
-    if cIdx%2 == 1 {
-        pIdx = (cIdx-1)/2
-    } else {
-        pIdx = (cIdx-2)/2
-    }
-    return pIdx
+	var pIdx int
+	if cIdx%2 == 1 {
+		pIdx = (cIdx - 1) / 2
+	} else {
+		pIdx = (cIdx - 2) / 2
+	}
+	return pIdx
 }
 
 func siftDown(h []int, parent int) {
-   sliceLen := len(h)
-   leftChild := (2*parent)+1;
-   for ; sliceLen > leftChild; {
-       rightChild := leftChild+1
-       swap := parent
-       if h[swap] < h[leftChild] {
-           swap = leftChild
-       }
-       if (rightChild < sliceLen) && (h[swap] < h[rightChild]) {
-           swap = rightChild
-       }
-       if swap == parent {
-           break
-       } else {
-           h[parent], h[swap] = h[swap], h[parent]
-           parent = swap
-           leftChild = (2*parent)+1
-       }
-   }
+	sliceLen := len(h)
+	leftChild := (2 * parent) + 1
+	for sliceLen > leftChild {
+		rightChild := leftChild + 1
+		swap := parent
+		if h[swap] < h[leftChild] {
+			swap = leftChild
+		}
+		if (rightChild < sliceLen) && (h[swap] < h[rightChild]) {
+			swap = rightChild
+		}
+		if swap == parent {
+			break
+		} else {
+			h[parent], h[swap] = h[swap], h[parent]
+			parent = swap
+			leftChild = (2 * parent) + 1
+		}
+	}
 }
 
-func MaxIntHeapify(h []int) bool {
-    sliceLen := len(h)
-    // early bail-out conditions
-    if sliceLen == 0 {
-        //stupid is as stupid does
-        return false
-    }
-    if sliceLen == 1 {
-        // it's easy to heapify a single element
-        return true
-    }
-    if sliceLen == 2 {
-        if h[0] < h[1] {
-            h[0], h[1] = h[1], h[0]
-        }
-        return true
-    }
+//MaxIntHeapify - take a slice of integers and turn it into a max heap
+func MaxIntHeapify(h []int) {
+	sliceLen := len(h)
+	if sliceLen < 2 {
+		// Either empty or a single element, this is a valid "heap"
+		return
+	}
+	if sliceLen == 2 {
+		if h[0] < h[1] {
+			h[0], h[1] = h[1], h[0]
+		}
+		return
+	}
 
-    lastIndex := sliceLen-1
-    for idx := getParentIndex(lastIndex); -1 < idx; idx-- {
-        siftDown(h, idx)
-    }
-    return true
+	lastIndex := sliceLen - 1
+	for idx := getParentIndex(lastIndex); -1 < idx; idx-- {
+		siftDown(h, idx)
+	}
+}
+
+//MaxIntHeapPush - add a value to a MaxIntHeap. The returned slice will be a
+//heap regardless of whether the initial parameter slice was one at the start.
+func MaxIntHeapPush(s []int, v int) []int {
+	h := append(s, v)
+	MaxIntHeapify(h)
+	return h
+}
+
+//MaxIntHeapPop - take the max value from the heap and re-heapify. If the
+//initial slice isn't a heap to start, you will get garbage out.
+func MaxIntHeapPop(s []int) (int, []int) {
+	cpy := make([]int, len(s), cap(s))
+	copy(cpy, s)
+	maxVal := cpy[0]
+	lastIndex := len(cpy) - 1
+	cpy[0] = cpy[lastIndex]
+	cpy = cpy[:lastIndex]
+	MaxIntHeapify(cpy)
+	return maxVal, cpy
+}
+
+//MaxIntHeapSort - take a slice of integers and sort it in place, max value last
+func MaxIntHeapSort(s []int) {
+	MaxIntHeapify(s) // just in case it's not a heap when we get it.
+	for idx := len(s) - 1; idx > 0; idx-- {
+		s[0], s[idx] = s[idx], s[0]
+		MaxIntHeapify(s[:idx])
+	}
 }
--- maxint_test.go
+++ maxint_test.go
@@ -1,59 +1,104 @@
 package heaputil
 
-import "testing"
+import (
+	"testing"
+
+	"reflect"
+)
 
 type testData struct {
-    testCase string
-    initial []int
-    final []int
-    success bool
+	testCase string
+	initial  []int
+	final    []int
 }
 
-var tests = []testData {
-    {"Single Element", []int{0}, []int{0}, true},
-    {"Two Elements Swap", []int{6, 27}, []int{27, 6}, true},
-    {"Two Elements No Swap", []int{6, 4}, []int{6, 4}, true},
-    {"Three Elements - 1", []int{1, 2, 3}, []int{3, 2, 1}, true},
-    {"Three Elements - 2", []int{1, 3, 2}, []int{3, 1, 2}, true},
-    {"Three Elements - 3", []int{3, 1, 2}, []int{3, 1, 2}, true},
-    {"Seven Elements - 1", []int{2,3,18,7,4,1,5}, []int{18,7,5,3,4,1,2}, true},
+var heapifyTests = []testData{
+	{"Empty Slice", []int{}, []int{}},
+	{"Single Element", []int{0}, []int{0}},
+	{"Two Elements Swap", []int{6, 27}, []int{27, 6}},
+	{"Two Elements No Swap", []int{6, 4}, []int{6, 4}},
+	{"Three Elements - 1", []int{1, 2, 3}, []int{3, 2, 1}},
+	{"Three Elements - 2", []int{1, 3, 2}, []int{3, 1, 2}},
+	{"Three Elements - 3", []int{3, 1, 2}, []int{3, 1, 2}},
+	{"Seven Elements - 1", []int{2, 3, 18, 7, 4, 1, 5}, []int{18, 7, 5, 3, 4, 1, 2}},
+	{"Eight Elements - 1", []int{6, 8, 14, 10, 18, 27, 5, 13}, []int{27, 18, 14, 13, 8, 6, 5, 10}},
 }
 
-func TestEmptySlice(t *testing.T){
-    mySlice := make([]int, 0)
-    success := MaxIntHeapify(mySlice)
-    if success {
-        t.Error("Heapify didn't fail with an empty slice.")
-    }
+func TestMaxIntHeapify(t *testing.T) {
+	for _, test := range heapifyTests {
+		t.Logf("Running %s", test.testCase)
+		testSlice := make([]int, len(test.initial))
+		copy(testSlice, test.initial)
+		MaxIntHeapify(testSlice)
+		if !reflect.DeepEqual(testSlice, test.final) {
+			t.Errorf("%s:\n\tExpected: %v\n\tActual: %v",
+				test.testCase, test.final, testSlice)
+		}
+	}
 }
 
-func sliceEquality(lhs, rhs []int) bool {
-    lenEq := false
-    valEq := true
-    if (len(lhs) == len(rhs)) {
-        lenEq = true
-        for i, _ := range lhs {
-            if lhs[i] != rhs[i] {
-                valEq = false
-                break
-            }
-        }
-    }
-    return lenEq && valEq
+var pushTests = []struct {
+	testCase string
+	pushVal  int
+	initial  []int
+	final    []int
+}{
+	{"Push append", 6, []int{27, 18, 14, 13, 8, 6, 5, 10}, []int{27, 18, 14, 13, 8, 6, 5, 10, 6}},
+	{"Push swap 1 level", 16, []int{27, 18, 14, 13, 8, 6, 5, 10}, []int{27, 18, 14, 16, 8, 6, 5, 10, 13}},
+	{"Push swap 2 levels", 22, []int{27, 18, 14, 13, 8, 6, 5, 10}, []int{27, 22, 14, 18, 8, 6, 5, 10, 13}},
+	{"Push swap new max", 33, []int{27, 18, 14, 13, 8, 6, 5, 10}, []int{33, 27, 14, 18, 8, 6, 5, 10, 13}},
 }
 
-func TestHeapify(t *testing.T){
-    for _, test := range tests {
-        t.Logf("Running %s", test.testCase)
-        testSlice := make([]int, len(test.initial))
-        copy(testSlice, test.initial)
-        res := MaxIntHeapify(testSlice)
-        if test.success != res {
-            t.Errorf("Test case %s returns an error.", test.testCase)
-        }
-        if ! sliceEquality(testSlice, test.final) {
-            t.Errorf("Test case %s:\n\tExpected: %v\n\tActual: %v")
-        }
-    }
+func TestMaxIntHeapPush(t *testing.T) {
+	for _, test := range pushTests {
+		t.Logf("Running %s", test.testCase)
+		result := MaxIntHeapPush(test.initial, test.pushVal)
+		if !reflect.DeepEqual(result, test.final) {
+			t.Errorf("%s:\n\tExpected: %v\n\tActual: %v",
+				test.testCase, test.final, result)
+		}
+	}
 }
 
+var popTests = []struct {
+	testCase string
+	initial  []int
+	popVal   int
+	final    []int
+}{
+	{"Pop max - 1", []int{27, 18, 14, 13, 8, 6, 5, 10}, 27, []int{18, 13, 14, 10, 8, 6, 5}},
+	{"Pop max - 2", []int{18, 7, 5, 3, 4, 1, 2}, 18, []int{7, 4, 5, 3, 2, 1}},
+}
+
+func TestMaxIntHeapPop(t *testing.T) {
+	for _, test := range popTests {
+		t.Logf("Running %s", test.testCase)
+		pop, heap := MaxIntHeapPop(test.initial)
+		if pop != test.popVal || !reflect.DeepEqual(heap, test.final) {
+			t.Errorf("%s:\n\tExpected: %d / %v\n\tActual: %d / %v",
+				test.testCase, test.popVal, test.final, pop, heap)
+		}
+	}
+}
+
+var sortTests = []struct {
+	testCase string
+	initial  []int
+	final    []int
+}{
+	{"Sort 1", []int{18, 7, 5, 3, 4, 1, 2}, []int{1, 2, 3, 4, 5, 7, 18}},
+	{"sort 2", []int{27, 18, 14, 13, 8, 6, 5, 10}, []int{5, 6, 8, 10, 13, 14, 18, 27}},
+}
+
+func TestMaxIntHeapSort(t *testing.T) {
+	for _, test := range sortTests {
+		t.Logf("Running %s", test.testCase)
+		testSlice := make([]int, len(test.initial))
+		copy(testSlice, test.initial)
+		MaxIntHeapSort(testSlice)
+		if !reflect.DeepEqual(testSlice, test.final) {
+			t.Errorf("%s:\n\tExpected: %v\n\tActual: %v",
+				test.testCase, test.final, testSlice)
+		}
+	}
+}
